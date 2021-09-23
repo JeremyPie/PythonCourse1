f1a9b60cce6aa07e27f6b16769616a55f949d019
JeremyPie <91102181+JeremyPie@users.noreply.github.com>
2021-09-24 00:15:13 +0300

    Update galaxy.py

--- UdemyMegaCourse/cv2/galaxy.py
+++ UdemyMegaCourse/cv2/galaxy.py
@@ -2,9 +2,8 @@ import cv2
 from matplotlib import pyplot as plt
 
 img = cv2.imread('galaxy.jpg', 0)
+img = cv2.resize(img, (int(img.shape[1]/2), int(img.shape[0]/2)))
+cv2.imwrite('GlaxyResized.jpg', img)
+
 plt.imshow(img)
 plt.show()
-
-cv2.imshow('Galaxy', img)
-cv2.waitkey(5000)
-cv2.destroyAllWindows()
